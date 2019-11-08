90a7b766663c515321243a640ebfd068d24522ea
Clara Gyökeres <claragyokeres@Claras-MacBook-Air.local>
2019-11-08 12:37:56 +0100

    body part

--- script.js
+++ script.js
@@ -9,18 +9,31 @@
 const apiKey = "b787e9052c508e6c2069f519f753dbff";
 const cityId = 61; //London
 const cuisineId = 60; //Japanese
+const cards = document.getElementById('cards')
+
+
 
 const url = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&cuisines=${cuisineId}`
 
-fetch(url, { headers: { "user-key": apiKey } })
+fetch(url, {
+    headers: {
+      "user-key": apiKey
+    }
+  })
   .then(res => res.json())
   .then(json => {
     console.log(json);
     json.restaurants.forEach(resto => {
-      document.getElementById("restaurants").innerHTML += `<li>${resto.restaurant.name}</li>`
-      document.getElementById("restaurants").innerHTML += `<li>${resto.restaurant.average_cost_for_two}</li>`
-      document.getElementById("restaurants").innerHTML += `<li>${resto.restaurant.location.address}</li>`
-      document.getElementById("restaurants").innerHTML += `<img src=${resto.restaurant.photos[0].photo.thumb_url}>`
-      document.getElementById("restaurants").innerHTML += `<li>${resto.restaurant.user_rating.aggregate_rating}</li>`
+      cards.innerHTML += `
+      <div class="card">
+          <img class ="photo" src=${resto.restaurant.photos[0].photo.thumb_url}>
+          <div class="nameRate">
+            <p>${resto.restaurant.name} </p> <p> ${resto.restaurant.user_rating.aggregate_rating}</p>
+          </div>
+          <p class="address">Address: ${resto.restaurant.location.address} </p>
+          <p class="average"> Average price: ${resto.restaurant.average_cost_for_two/2} £</p>
+
+      </div>
+      `
     });
   });
